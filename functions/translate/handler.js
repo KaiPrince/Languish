ad192912c7cf08500057dee7cddf2f7ac6548a45
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2022-05-10 15:06:54 -0400

    Get text from body.

--- functions/translate/handler.js
+++ functions/translate/handler.js
@@ -2,11 +2,12 @@
 const translate = require("./translate");
 
 module.exports.hello = async (event) => {
+  const text = event?.body ?? "hello";
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: await translate("hello"),
+        message: await translate(text),
         input: event,
       },
       null,
