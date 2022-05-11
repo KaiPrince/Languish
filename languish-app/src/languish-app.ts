268d45ef93b15628ba358181736d54f2929f0731
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2022-05-11 13:33:18 -0400

    A little bit of styling

--- languish-app/src/languish-app.ts
+++ languish-app/src/languish-app.ts
@@ -2,8 +2,6 @@ import { LitElement, html, css } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { TRANSLATION_URL } from './const.js';
 
-const logo = new URL('../../assets/open-wc-logo.svg', import.meta.url).href;
-
 @customElement('languish-app')
 export class LanguishApp extends LitElement {
   @property()
@@ -18,14 +16,20 @@ export class LanguishApp extends LitElement {
       justify-content: flex-start;
       font-size: calc(10px + 2vmin);
       color: #1a2b42;
-      max-width: 960px;
+      /* max-width: 960px; */
+      width: 100%;
       margin: 0 auto;
       text-align: center;
       background-color: var(--languish-app-background-color);
+      font-family: 'Comic Sans MS';
     }
 
     main {
       flex-grow: 1;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
 
     .logo {
@@ -58,31 +62,75 @@ export class LanguishApp extends LitElement {
       border: 2px solid white;
       border-radius: 5px;
     }
+
+    .col {
+      display: flex;
+      flex-direction: column;
+    }
+    .row {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .speed {
+      flex-basis: 4;
+    }
+
+    .language {
+      flex-basis: 8;
+    }
+
+    .highlight-text {
+      color: red;
+    }
+
+    .placeholder-text {
+      color: grey;
+    }
   `;
 
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
-        <p>The world's worst way to learn a language.</p>
-
-        <select @change=${this._handleLangChange}>
-          <option value="fr">French</option>
-          <option value="es">Spanish</option>
-          <option value="de">German</option>
-          <option value="sr">Serbian</option>
-        </select>
-
-        <div class="translation-row">
-          <textarea cols="30" rows="10" @input=${this._handleInput}></textarea>
-          ${this._translation == null
-            ? html`<p>
-                ${this._isLoading
-                  ? 'Translation is loading...'
-                  : 'Translated text will appear here.'}
-              </p>`
-            : html`<p lang=${this._targetLang}>${this._translation}</p>`}
+        <div class="row">
+          <div class="col">
+            <h1 class="title">${this.title}</h1>
+            <p>The world's worst way to learn a language.</p>
+            <div id="spritz-source-lang" class="spritz-reader">
+              T<span class="highlight-text">h</span>ey
+            </div>
+            <div id="spritz-target-lang" class="spritz-reader">
+              El<span class="highlight-text">l</span>os
+              ${this._translation == null
+                ? html`<small class="placeholder-text">
+                    ${this._isLoading
+                      ? 'Translation is loading...'
+                      : 'Translated text will appear here.'}
+                  </small>`
+                : html`<span lang=${this._targetLang}
+                    >${this._translation}</span
+                  >`}
+            </div>
+            <div class="row">
+              <div class="speed">250 wpm</div>
+              <div class="language">
+                <select @change=${this._handleLangChange}>
+                  <option value="fr">French</option>
+                  <option value="es">Spanish</option>
+                  <option value="de">German</option>
+                  <option value="sr">Serbian</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <textarea
+              cols="30"
+              rows="10"
+              @input=${this._handleInput}
+              placeholder="Paste your text here"
+            ></textarea>
+          </div>
         </div>
 
         <button
