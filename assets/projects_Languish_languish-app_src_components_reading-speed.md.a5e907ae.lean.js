49b88554b427bc23af35c55bf93585ec665c32db
KaiPrince <KaiPrince@users.noreply.github.com>
2022-05-16 16:59:46 +0000

    Deploying to gh-pages from @ KaiPrince/Languish@7cb5cbdaa03abfc7f547568dc07aa8e0b661c3a6 🚀

---
+++ assets/projects_Languish_languish-app_src_components_reading-speed.md.a5e907ae.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a}from"./app.ba656c22.js";const t='{"title":"","description":"","frontmatter":{},"relativePath":"projects/Languish/languish-app/src/components/reading-speed.md","lastUpdated":1652720378219}',p={},o=a('',2);p.render=function(a,t,p,e,c,l){return n(),s("div",null,[o])};export default p;export{t as __pageData};
